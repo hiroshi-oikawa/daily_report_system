d5001c519d07645cb725489a5ccdbc1fe51ccd9c
hiroshi.oikawa <hiroshijichuan0@gmail.com>
2022-04-25 18:38:09 +0900

    従業員indexアクションの作成、その他ファイルの修正

--- src/main/java/models/validators/EmployeeValidator.java
+++ src/main/java/models/validators/EmployeeValidator.java
@@ -7,8 +7,20 @@ import actions.views.EmployeeView;
 import constants.MessageConst;
 import services.EmployeeService;
 
+/**
+ * 従業員インスタンスに設定されている値のバリデーションを行うクラス
+ *
+ */
 public class EmployeeValidator {
 
+    /**
+     * 従業員インスタンスの各項目についてバリデーションを行う
+     * @param service 呼び出し元Serviceクラスのインスタンス
+     * @param ev EmployeeViewのインスタンス
+     * @param codeDuplicateCheckFlag 社員番号の重複チェックを実施するかどうか(実施する:true 実施しない:false)
+     * @param passwordCheckFlag パスワードの入力チェックを実施するかどうか(実施する:true 実施しない:false)
+     * @return エラーのリスト
+     */
     public static List<String> validate(
             EmployeeService service, EmployeeView ev, Boolean codeDuplicateCheckFlag, Boolean passwordCheckFlag) {
         List<String> errors = new ArrayList<String>();
@@ -34,9 +46,13 @@ public class EmployeeValidator {
         return errors;
     }
 
-
-
-
+    /**
+     * 社員番号の入力チェックを行い、エラーメッセージを返却
+     * @param service EmployeeServiceのインスタンス
+     * @param code 社員番号
+     * @param codeDuplicateCheckFlag 社員番号の重複チェックを実施するかどうか(実施する:true 実施しない:false)
+     * @return エラーメッセージ
+     */
     private static String validateCode(EmployeeService service, String code, Boolean codeDuplicateCheckFlag) {
 
         //入力値がなければエラーメッセージを返却
@@ -59,26 +75,46 @@ public class EmployeeValidator {
         return "";
     }
 
-
+    /**
+     * @param service EmployeeServiceのインスタンス
+     * @param code 社員番号
+     * @return 従業員テーブルに登録されている同一社員番号のデータの件数
+     */
     private static long isDuplicateEmployee(EmployeeService service, String code) {
-        long emploueesCount = service.countByCode(code);
+
+        long employeesCount = service.countByCode(code);
         return employeesCount;
     }
 
-
+    /**
+     * 氏名に入力値があるかをチェックし、入力値がなければエラーメッセージを返却
+     * @param name 氏名
+     * @return エラーメッセージ
+     */
     private static String validateName(String name) {
+
         if (name == null || name.equals("")) {
-            return MessageCount.E_NONAME.getMassage();
+            return MessageConst.E_NONAME.getMessage();
         }
 
+        //入力値がある場合は空文字を返却
         return "";
-
     }
 
-    private static String ValidatePassword(String password, Boolean passwordCheckFlag) {
-        if(passwordCheckFlag && (password == null || password.equals(""))) {
-            return MessageCount.E_NPPASSWORD.getMessage();
+    /**
+     * パスワードの入力チェックを行い、エラーメッセージを返却
+     * @param password パスワード
+     * @param passwordCheckFlag パスワードの入力チェックを実施するかどうか(実施する:true 実施しない:false)
+     * @return エラーメッセージ
+     */
+    private static String validatePassword(String password, Boolean passwordCheckFlag) {
+
+        //入力チェックを実施 かつ 入力値がなければエラーメッセージを返却
+        if (passwordCheckFlag && (password == null || password.equals(""))) {
+            return MessageConst.E_NOPASSWORD.getMessage();
         }
+
+        //エラーがない場合は空文字を返却
         return "";
     }
 }
